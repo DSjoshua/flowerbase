320177759bd74df5196770893b1a4115c8978111
Beastmode <113318548+DSjoshua@users.noreply.github.com>
2024-03-05 06:05:40 -0800

    a

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("open-modal-left").addEventListener("click", function () {
+    // Redirect to the desired website
+    window.location.href = "https://notes-love.netlify.app/"; // Replace "https://example.com" with the URL you want to redirect to
+  });
+
+    // Event listener for right message button
+    document.getElementById("open-modal-right").addEventListener("click", function () {
+    // Redirect to the desired website
+    window.location.href = "https://bird-love.netlify.app/"; // Replace "https://example.com" with the URL you want to redirect to
+  });
+
+  // JavaScript to create falling hearts
+  const container = document.querySelector(".falling-hearts");
+
+  function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("falling-heart");
+    heart.innerHTML = "&#10084;"; // Unicode for heart
+    heart.style.left = Math.random() * window.innerWidth + "px";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s"; // Randomize animation duration
+    container.appendChild(heart);
+    setTimeout(() => {
+      heart.remove();
+    }, 5000); // Remove heart after 5 seconds
+  }
+
+  setInterval(createHeart, 200);
